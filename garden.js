efd5af45df84b86fc8148f4340cbc0c1f65c0a24
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2023-04-12 23:22:47 +1200

    Update garden.js

--- garden.js
+++ garden.js
@@ -1,3 +1,4 @@
+// Array of model objects with their source, location, and scale information
 const models = [
     {
         src: 'model-one.glb',
@@ -11,43 +12,53 @@ const models = [
         longitude: 77.5895627,
         scale: '1.5 1.5 1.5'
     }
-            // Add more models with their corresponding locations here
-        ];
 
-        const distanceThreshold = 0.01; // Adjust this value based on the range (in degrees) you want to consider for displaying the model
+    // Add more models with their corresponding locations and scales here
+];
 
-        function loadModelIfNearby(position) {
-            const currentLatitude = position.coords.latitude;
-            const currentLongitude = position.coords.longitude;
+// Set a threshold for the distance (in degrees) that will be considered for displaying the model
+const distanceThreshold = 0.01;
 
-            let modelLoaded = false;
+// Function to load a 3D model if the user's location is near one of the specified locations
+function loadModelIfNearby(position) {
+    const currentLatitude = position.coords.latitude;
+    const currentLongitude = position.coords.longitude;
 
-            for (const model of models) {
-                if (Math.abs(currentLatitude - model.latitude) <= distanceThreshold &&
-                    Math.abs(currentLongitude - model.longitude) <= distanceThreshold) {
-                    
-                    const modelViewer = document.querySelector('model-viewer');
-                    modelViewer.src = model.src;
-                    modelViewer.style.display = 'block';
+    let modelLoaded = false;
 
-                    modelViewer.addEventListener('load', () => {
-                        modelViewer.activateAR();
-                    });
+    // Iterate through the models array and check if the user's location is close enough to any model's location
+    for (const model of models) {
+        if (Math.abs(currentLatitude - model.latitude) <= distanceThreshold &&
+            Math.abs(currentLongitude - model.longitude) <= distanceThreshold) {
+            
+            const modelViewer = document.querySelector('model-viewer');
+            modelViewer.src = model.src; // Set the source of the model to be displayed
+            modelViewer.style.display = 'block'; // Make the model visible
+            modelViewer.setAttribute('scale', model.scale); // Set the scale for the loaded model
 
-                    modelLoaded = true;
+            // Activate AR mode once the model is loaded
+            modelViewer.addEventListener('load', () => {
+                modelViewer.activateAR();
+            });
 
-                    // Stop checking other models once a model is loaded
-                    break;
-                }
-            }
+            modelLoaded = true;
 
-            if (!modelLoaded) {
-                alert('Sorry, you are not close enough to any of the desired locations to load the 3D models.');
-            }
+            // Stop checking other models once a model is loaded
+            break;
         }
+    }
 
-        function error(err) {
-            console.warn(`ERROR(${err.code}): ${err.message}`);
-        }
+    // Show an alert if the user is not close enough to any of the desired locations to load the 3D models
+    if (!modelLoaded) {
+        alert('You are not close enough to any of the desired locations to load the 3D models.');
+    }
+}
 
-        navigator.geolocation.getCurrentPosition(loadModelIfNearby, error);
+// Function to handle geolocation errors
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+// Get the user's current position and call the loadModelIfNearby function if successful, otherwise call the error function
+navigator.geolocation.getCurrentPosition(loadModelIfNearby, error);
+
